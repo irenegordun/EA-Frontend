99cd69586d76a11d5774a75057ac595d565cef10
Alba Onrubia <alba.onrubia2@estudiantat.upc.edu>
2022-12-12 21:10:52 +0100

    unexpected null debug

--- flutter_front/lib/views/ListParkings.dart
+++ flutter_front/lib/views/ListParkings.dart
@@ -20,11 +20,10 @@ class ListParkings extends StatefulWidget {
 
   @override
   State<ListParkings> createState() => _ListParkingsState();
-  
 }
 
 class _ListParkingsState extends State<ListParkings> {
-  List<Parking>? parkings;
+  List<Parking>? parkings = [];
   var isLoaded = false;
 
   @override
@@ -32,6 +31,7 @@ class _ListParkingsState extends State<ListParkings> {
     super.initState();
     getData();
   }
+
   getData() async {
     parkings = await ParkingServices().getParkings();
     if (parkings != null) {
@@ -40,135 +40,120 @@ class _ListParkingsState extends State<ListParkings> {
       });
     }
   }
+
   @override
   Widget build(BuildContext context) {
-    
-
     ParkingServices _parkingprovider = Provider.of<ParkingServices>(context);
 
     return Scaffold(
       drawer: const DrawerScreen(),
-      floatingActionButton :const AccessibilityButton(),
+      floatingActionButton: const AccessibilityButton(),
       appBar: AppBar(
         title: new Center(
           child: new Text("A P A R C A ' M"),
         ),
         backgroundColor: Colors.blueGrey,
       ),
-      
       body: Column(
         children: <Widget>[
-          
-
-        Expanded(
-          child: Row(children: <Widget>[
           Expanded(
-              child: Container(
-                  color: Color.fromARGB(255, 227, 244, 248),
-                  child: const Center(
-                      child: Text("Calendar",
-                          style: TextStyle(fontSize: 20.0))))),
-          Expanded(
-              child: GestureDetector(
-                onTap:(){
-                  Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const Filters()));             
-                },
+              child: Row(children: <Widget>[
+            Expanded(
                 child: Container(
+                    color: Color.fromARGB(255, 227, 244, 248),
+                    child: const Center(
+                        child: Text("Calendar",
+                            style: TextStyle(fontSize: 20.0))))),
+            Expanded(
+                child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const Filters()));
+              },
+              child: Container(
                 color: Colors.blueGrey,
                 child: const Center(
-                child: Text("Filters",
-                  style: TextStyle(fontSize: 20.0))
-                ),
-                  
+                    child: Text("Filters", style: TextStyle(fontSize: 20.0))),
               ),
-              )
+            )),
+          ])),
 
-              ),
-        ])), 
+          Expanded(
+              child: Row(children: <Widget>[
+            Expanded(
+                child: Container(
+                    height: 40,
+                    width: 50,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('List'),
+                        style: const ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll<Color>(Colors.blueGrey),
+                        )))),
+            Expanded(
+                child: Container(
+                    height: 40,
+                    width: 50,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const MapParkings()));
+                        },
+                        child: const Text('Map'),
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll<Color>(Colors.blueGrey),
+                        )))),
+          ])),
 
-        Expanded(
-            child: Row(children: <Widget>[
+          //Row 2/2
           Expanded(
-              child: Container(
-                  height: 40,
-                  width: 50,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('List'),
-                    style: const ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey),
-                    )
-                  )
-                  )),
-          Expanded(
-              child: Container(
-                  height: 40,
-                  width: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const MapParkings())); 
-                    },
-                    child: const Text('Map'),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey),
-                    )
-                  )
-                          )),
-        ])), 
-        
-        //Row 2/2
-        Expanded(
             child: ListView.builder(
-            itemCount: parkings?.length,
-            itemBuilder: (context, index) {
-              return Card(
-                color: Color.fromARGB(255, 144, 180, 199),
-                child: ListTile(
-                  leading: Container(
-                    width: 80,
-                    height: 80,
-                    child: Image.asset(
-                        'parking1.jpg'),
-                  ),
-                  
-                  title: Text(parkings![index].street),
-                  subtitle: Text(parkings![index].city),
-                  trailing: SizedBox(
-                      width: 120,
-
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: IconButton(
-                              icon: const Icon(Icons.favorite_border_outlined),
-                              tooltip: 'Add to favourites',
-                              onPressed: () {
-                              },
-                            ),
-                          ),
-                          Expanded(
+              itemCount: parkings?.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Color.fromARGB(255, 144, 180, 199),
+                  child: ListTile(
+                    leading: Container(
+                      width: 80,
+                      height: 80,
+                      child: Image.asset('parking1.jpg'),
+                    ),
+                    title: Text(parkings![index].street),
+                    subtitle: Text(parkings![index].city),
+                    trailing: SizedBox(
+                        width: 120,
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
                               child: IconButton(
-                            icon: const Icon(Icons.info_outline),
-                            tooltip: 'More Info',
-                            onPressed: () {
-                              _parkingprovider.setParkingData(parkings![index]);
+                                icon:
+                                    const Icon(Icons.favorite_border_outlined),
+                                tooltip: 'Add to favourites',
+                                onPressed: () {},
+                              ),
+                            ),
+                            Expanded(
+                                child: IconButton(
+                              icon: const Icon(Icons.info_outline),
+                              tooltip: 'More Info',
+                              onPressed: () {
+                                _parkingprovider
+                                    .setParkingData(parkings![index]);
                                 Navigator.of(context).push(MaterialPageRoute(
                                     builder: (context) => const ParkingInfo()));
-                            },
-                          )),   
-                        ],
-                      )),
-                ),
-              );
-            },
-          ),), 
-         
-          
-        ],      
+                              },
+                            )),
+                          ],
+                        )),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
