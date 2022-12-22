f055bfc7de2966431cc0de761acf598005236f65
Aida Padilla <aida.padilla.gonzalez.aiko@gmail.com>
2022-12-22 15:11:10 +0100

    mapas

--- flutter_front/lib/views/MapParkings.dart
+++ flutter_front/lib/views/MapParkings.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -56,66 +58,72 @@ class _MapParkingsState extends State<MapParkings> {
       body: Column(
         children: <Widget>[
           Expanded(
+              flex: 1,
               child: Row(children: <Widget>[
-            Expanded(
-                child: Container(
-                    color: Color.fromARGB(255, 227, 244, 248),
+                Expanded(
+                    child: Container(
+                        color: Color.fromARGB(255, 227, 244, 248),
+                        child: const Center(
+                            child: Text("Calendar",
+                                style: TextStyle(fontSize: 20.0))))),
+                Expanded(
+                    child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const Filters()));
+                  },
+                  child: Container(
+                    color: Colors.blueGrey,
                     child: const Center(
-                        child: Text("Calendar",
-                            style: TextStyle(fontSize: 20.0))))),
-            Expanded(
-                child: GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const Filters()));
-              },
-              child: Container(
-                color: Colors.blueGrey,
-                child: const Center(
-                    child: Text("Filters", style: TextStyle(fontSize: 20.0))),
-              ),
-            )),
-          ])),
+                        child:
+                            Text("Filters", style: TextStyle(fontSize: 20.0))),
+                  ),
+                )),
+              ])),
           Expanded(
+              flex: 1,
               child: Row(children: <Widget>[
-            Expanded(
-                child: Container(
-                    height: 40,
-                    width: 50,
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => const ListParkings()));
-                        },
-                        child: const Text('List'),
-                        style: const ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll<Color>(Colors.blueGrey),
-                        )))),
-            Expanded(
-                child: Container(
-              height: 40,
-              width: 50,
-              child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Map'),
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStatePropertyAll<Color>(Colors.blueGrey),
-                  )),
-            )),
-          ])),
+                Expanded(
+                    child: Container(
+                        height: 60,
+                        width: 50,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => const ListParkings()));
+                            },
+                            child: const Text('List'),
+                            style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll<Color>(
+                                  Colors.blueGrey),
+                            )))),
+                Expanded(
+                    child: Container(
+                  height: 60,
+                  width: 50,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: const ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll<Color>(Colors.blueGrey),
+                      ),
+                      child: const Text('Map')),
+                )),
+              ])),
           Expanded(
+              flex: 9,
               child: Row(children: <Widget>[
-            Expanded(
-                child: FlutterOpenStreetMap(
-                    center: LatLong(5, 10),
-                    onPicked: (pickedData) {
-                      print(pickedData.latLong.latitude);
-                      print(pickedData.latLong.longitude);
-                      print(pickedData.address);
-                    }))
-          ]))
+                Expanded(
+                    child: FlutterOpenStreetMap(
+                        center: LatLong((41.3948), (2.1596)),
+                        showZoomButtons: true,
+                        onPicked: (pickedData) {
+                          const Point(41.3948, 2.1596);
+                          // print(pickedData.latLong.latitude);
+                          // print(pickedData.latLong.longitude);
+                          // print(pickedData.address);
+                        }))
+              ]))
         ],
       ),
     );
